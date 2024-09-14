c82d37274290f4ebbfe729575c723848de46d7df
nvkoval <nvkoval@gmail.com>
2024-09-14 21:35:01 +0300

    Convert model.qmd to .py

--- model.py
+++ model.py
@@ -1,11 +1,3 @@
----
-title: "Model"
-format:
-    html:
-        code-fold: true
----
-
-```{python}
 import duckdb
 from pandas import get_dummies
 from sklearn.linear_model import LinearRegression
@@ -14,54 +6,39 @@ from vetiver import VetiverModel
 from pins import board_folder
 from vetiver import vetiver_pin_write
 from vetiver import VetiverAPI
-```
 
-## Get Data
-
-```{python}
+# Get Data
 con = duckdb.connect('my-db.duckdb')
 df = con.execute('SELECT * FROM penguins').fetchdf().dropna()
 con.close()
 
 df.head(3)
-```
 
-## Define Model and Fit
-```{python}
+
+# Define Model and Fit
 X = get_dummies(df[["bill_length_mm", "species", "sex"]], drop_first=True)
 y = df["body_mass_g"]
 
 model = LinearRegression().fit(X, y)
-```
 
-## Get some information
-```{python}
+# Get some information
 print(f"R^2 {model.score(X, y):.4f}")
 print(f"Intercept {model.intercept_:.4f}")
 print(f"Columns {X.columns}")
 print(f"Coefficients {model.coef_}")
-```
 
-## Turn into Vetiver Model
-
-```{python}
+# Turn into Vetiver Model
 v = VetiverModel(model, model_name='penguin_model', prototype_data=X)
-```
 
-## Save to Board
 
-```{python}
+# Save to Board
 model_board = board_folder(
     "/data/model",
-    allow_pickle_read = True
+    allow_pickle_read=True
 )
 
 vetiver_pin_write(model_board, v)
-```
 
-## Turn model into API
-
-```{python}
+# Turn model into API
 v_api = VetiverAPI(v, check_prototype=True)
 # v_api.run(port=8080)
-```
